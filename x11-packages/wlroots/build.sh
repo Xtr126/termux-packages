727e5d14e2c4b3aafb9731bd6e1e92cf917143af
Xtr126 <k.gunetilleke@gmail.com>
2025-07-01 21:52:49 +0530

    Remove termux-gui from wlroots

--- x11-packages/wlroots/build.sh
+++ x11-packages/wlroots/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION="0.18.2"
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/${TERMUX_PKG_VERSION}/wlroots-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=a28061f7778f28f7be377fd4d6f0ebd58cb2a63b52460e9fde28e2ab43e80cab
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libdrm, libglvnd, libpixman, libwayland, libxcb, libxkbcommon, xcb-util-renderutil, xcb-util-wm, termux-gui-c"
+TERMUX_PKG_DEPENDS="libdrm, libglvnd, libpixman, libwayland, libxcb, libxkbcommon, xcb-util-renderutil, xcb-util-wm"
 TERMUX_PKG_BUILD_DEPENDS="libglvnd-dev, libwayland-cross-scanner, libwayland-protocols, xwayland"
 TERMUX_PKG_RECOMMENDS="xwayland"
 TERMUX_PKG_BREAKS="sway (<< 1.10 )"
@@ -14,13 +14,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dexamples=false
 -Dxwayland=enabled
 -Dsession=disabled
--Dbackends=x11,termuxgui
+-Dbackends=x11
 -Drenderers=gles2,vulkan
 "
-
-termux_step_post_get_source() {
-	cp -r $TERMUX_PKG_BUILDER_DIR/src/* $TERMUX_PKG_SRCDIR/
-}
 
 termux_step_pre_configure() {
 	termux_setup_wayland_cross_pkg_config_wrapper
